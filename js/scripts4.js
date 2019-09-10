94fa76b34d719c8499522dde3437f6a6f408c9be
Arnaud Ardans II and Angela Weber <angela@>
2019-09-09 20:41:04 -0700

    Fixed fadeToggle, and added 3 additional jQuery practice projects.

---
+++ js/scripts4.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+
+    $(".name").text(nameInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
